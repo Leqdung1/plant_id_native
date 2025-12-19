4d0ef269f25c04d71455f8ce166a1648b45f3e89
Leqdung1 <lequangdungkl@gmail.com>
2025-12-19 17:01:05 +0700

    add premium bềnit

---
+++ app/src/main/java/com/example/myapplication/screen/premium/BenefitData.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.screen.premium
+
+import androidx.compose.ui.graphics.painter.Painter
+
+data class BenefitData(
+    val icon : Painter,
+    val title : String,
+)
--- app/src/main/java/com/example/myapplication/screen/premium/PremiumBenefit.kt
+++ app/src/main/java/com/example/myapplication/screen/premium/PremiumBenefit.kt
@@ -1,3 +1,47 @@
 package com.example.myapplication.screen.premium
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
+
+@Composable
+fun PremiumBenefit() {
+    val benefit = listOf(
+        BenefitData(
+            icon = painterResource(id = R.drawable.ic_chat),
+            title = "No Ads"
+        )
+    )
+}
+
+@Composable
+fun ItemBenefit(
+    icon: Painter,
+    title: String,
+) {
+    Row(
+        modifier = Modifier.padding(24.dp)
+    ) {
+        Icon(
+            painter = icon,
+            contentDescription = null,
+            modifier = Modifier.weight(1f)
+        )
+        Spacer(
+            modifier = Modifier.width(12.dp)
+        )
+        Text(
+            title,
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
